5400d220de011418c5adae7550e062f57d8aa47d
Otto Micheletti <michelettiotto@gmail.com>
2021-12-02 16:53:32 -0300

    Desafio 4 - ok

--- src/challenges.js
+++ src/challenges.js
@@ -22,8 +22,9 @@ function splitSentence(string) {
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
+function concatName(array) {
+  let frase = [array[array.length - 1], array[0]];
+  return frase.join(', ');
 }
 
 // Desafio 5
